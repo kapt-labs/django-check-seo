bb8e2d805b1b34b7819a64c9675f9add4438d78d
cb <cb@kapt.mobi>
2019-10-30 17:03:10 +0100

    change template view, add new checks
    check 7: keyword is present in url
    check 8: h1 is present exactly 1 time
    check 9: keyword is present in h1

--- django-check-seo/views.py
+++ django-check-seo/views.py
@@ -27,14 +27,14 @@ class IndexView(generic.base.TemplateView):
 
         context["settings"] = json.dumps(settings.SEO_SETTINGS, indent=4)
 
-        django_check_seo = DjangoCheckSeo(soup)
+        django_check_seo = DjangoCheckSeo(soup, full_url)
         (context["problems"], context["warnings"]) = django_check_seo.check()
 
         return context
 
 
 class DjangoCheckSeo:
-    def __init__(self, soup):
+    def __init__(self, soup, full_url):
         """Populate some vars.
 
         Arguments:
@@ -45,6 +45,7 @@ class DjangoCheckSeo:
         self.content = self.soup.find("div", {"class": "container"})
         # remove ul with nav class from content (<ul class="nav"> is the menu)
         self.content.find("ul", {"class": "nav"}).extract()
+        self.full_url = full_url
         self.keywords = []
         self.problems = []
         self.warnings = []
@@ -54,6 +55,8 @@ class DjangoCheckSeo:
         self.check_title()
         self.check_links()
         self.check_keyword_occurence()
+        self.check_keyword_url()
+        self.check_h1()
 
         return (self.problems, self.warnings)
 
@@ -207,7 +210,7 @@ class DjangoCheckSeo:
 
     def check_keyword_occurence(self):
         """Check if one of the keywords is present between keywords_repeat[0] & keywords_repeat[1] in the page. If no keywords is in this range, then will fire a problem.
-        There is no case here (keyword & text are lowered before comparison).
+        no case sensitive (keyword & text are lowered before comparison).
         Thx https://stackoverflow.com/a/17268979/6813732 for finditer.
         """
         occurence = []
@@ -249,6 +252,73 @@ class DjangoCheckSeo:
                         ),
                         "description": _(
                             "Some SEO websites advise you to get 1% of your words to be keywords. For other websites (like Yoast) it's 0.25-0.5%. We use a constant for keywords repetition. Too many keywords on a page will lead search engines to think that you're doing some keyword stuffing (put too many keywords in order to manipulate the page rank)."
+                        ),
+                    }
+                )
+
+    def check_keyword_url(self):
+        """Check presence of keywords in url
+        """
+        for keyword in self.keywords:
+            if keyword in self.full_url:
+                return
+        self.problems.append(
+            {
+                "name": _("No keyword in URL"),
+                "settings": _("at least 1"),
+                "description": _(
+                    'Keywords in URL are a small ranking factor for Google (<a href="https://twitter.com/JohnMu/status/1070634500022001666">source</a>), but it will help your users understand the organisation of your website (/?product=50 talk less than /products/camping/). On the other hand Bing says : "<i>URL structure and keyword usage - keep it clean and keyword rich when possible</i>" (<a href="https://www.bing.com/webmaster/help/webmaster-guidelines-30fba23a">source</a>).'
+                ),
+            }
+        )
+
+    def check_h1(self):
+        """Check all h1-related conditions
+        """
+
+        h1 = self.soup.find_all("h1")
+        if len(h1) > 1:
+            self.problems.append(
+                {
+                    "name": _("Too much h1 tags"),
+                    "settings": _("exactly 1"),
+                    "description": _(
+                        'Google has told that they do not consider using multiple h1 a bad thing (<a href="https://www.youtube.com/watch?v=WsgrSxCmMbM">source</a>), but Google is not the unique search engine out there. Bing webmaster guidelines says "Use only one <H1> tag per page".'
+                    ),
+                }
+            )
+
+        elif not h1:
+            self.problems.append(
+                {
+                    "name": _("No h1 tag"),
+                    "settings": _("exactly 1"),
+                    "description": _(
+                        "H1 is the most visually notable content of your page for your users, and is one of the most important ranking factor for search engines. A good h1 tag content is required in order to progress in SERP."
+                    ),
+                }
+            )
+
+        else:
+            occurence = []
+            for keyword in self.keywords:
+                occurence.append(
+                    sum(
+                        1
+                        for _ in re.finditer(
+                            r"\b%s\b" % re.escape(keyword.lower()),
+                            self.content.text.lower(),
+                        )
+                    )
+                )
+            # if no keyword is found in h1
+            if not any(i > 0 for i in occurence):
+                self.problems.append(
+                    {
+                        "name": _("No keyword in h1"),
+                        "settings": _("at least 1"),
+                        "description": _(
+                            "H1 are crawled by search engines as the title of your page. You may populate them with appropriate content in order to be sure that search engines correctly understand what your pages are all about."
                         ),
                     }
                 )
