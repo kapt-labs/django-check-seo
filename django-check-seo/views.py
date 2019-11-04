00cb884b87d54161f4b8588342a8c4c4af7a85e4
cb <cb@kapt.mobi>
2019-11-04 16:55:06 +0100

    add new check, remove stop-words
    check 17: count words in main content

--- django-check-seo/views.py
+++ django-check-seo/views.py
@@ -5,14 +5,12 @@ import re
 
 # Third party
 from bs4 import BeautifulSoup
-from django.utils.translation import get_language, gettext as _
+from django.utils.translation import gettext as _
 from django.views import generic
 import requests
-from stop_words import get_stop_words
 
 # Local application / specific library imports
 from .conf import settings
-from .google_stop_words import get_stop_words as gsw
 
 
 class IndexView(generic.base.TemplateView):
@@ -518,17 +516,40 @@ class DjangoCheckSeo:
         )
 
     def count_words_without_stopwords(self):
-        language = get_language()
-        stop_words = get_stop_words(language) + gsw.get_stop_words(language)
+        """[summary]
+        """
+
         content = re.findall(r"\w+", self.content.text.lower())
 
         nb_words = len(content)
 
-        print(nb_words)
+        # too few words
+        if nb_words < settings.SEO_SETTINGS["content_words_number"][0]:
+            self.problems.append(
+                {
+                    "name": _("Content is too short"),
+                    "settings": "at least {min} words, more than {min2} if possible, found {nb_words}".format(
+                        min=settings.SEO_SETTINGS["content_words_number"][0],
+                        min2=settings.SEO_SETTINGS["content_words_number"][1],
+                        nb_words=nb_words,
+                    ),
+                    "description": _(
+                        'Yoast provide us some knowledge : "A blog post should contain at least 300 words in order to rank well in the search engines. Long posts will rank more easily than short posts. However, long posts require strong writing skills" (<a href="https://yoast.com/blog-post-length/">source</a>).<br />An article from Forbes from 2017 says that "<i>content with 1,000 words or more tends to attract significantly more links and shares</i>", and "<i>the average content length for top 3 rankings was about 750 words, while the average content length for position 20 rankings was about 500 words</i>" (<a href="https://web.archive.org/web/20190708230659/http://www.forbes.com/">source</a>).'
+                    ),
+                }
+            )
 
-        for stop_word in stop_words:
-            nb_words -= content.count(stop_word)
-            content = list(filter((stop_word).__ne__, content))
-
-        print(content)
-        print(nb_words)
+        elif nb_words < settings.SEO_SETTINGS["content_words_number"][1]:
+            self.warnings.append(
+                {
+                    "name": _("Content is too short"),
+                    "settings": "at least {min} words, more than {min2} if possible, found {nb_words}".format(
+                        min=settings.SEO_SETTINGS["content_words_number"][0],
+                        min2=settings.SEO_SETTINGS["content_words_number"][1],
+                        nb_words=nb_words,
+                    ),
+                    "description": _(
+                        'Yoast provide us some knowledge : "A blog post should contain at least 300 words in order to rank well in the search engines. Long posts will rank more easily than short posts. However, long posts require strong writing skills" (<a href="https://yoast.com/blog-post-length/">source</a>).<br />An article from Forbes from 2017 says that "<i>content with 1,000 words or more tends to attract significantly more links and shares</i>", and "<i>the average content length for top 3 rankings was about 750 words, while the average content length for position 20 rankings was about 500 words</i>" (<a href="https://web.archive.org/web/20190708230659/https://www.forbes.com/sites/jaysondemers/2017/07/18/how-long-should-your-content-be-for-optimal-seo/2">source</a>).'
+                    ),
+                }
+            )
