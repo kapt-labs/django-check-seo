8adff0b5364d8c811a6d240af4a0335089ac5958
cb <cb@kapt.mobi>
2019-12-04 10:15:56 +0100

    fix #18
    access to content is more secure

--- django-check-seo/urls.py
+++ django-check-seo/urls.py
@@ -1,8 +1,9 @@
 # Third party
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 # Local application / specific library imports
 from . import views
 
 
-urlpatterns = [path("", views.IndexView.as_view(), name="Index")]
+urlpatterns = [path("", staff_member_required(views.IndexView.as_view()), name="Index")]
