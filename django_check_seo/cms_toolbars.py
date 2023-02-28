229d10eed1eea78ed0076f261c0c9d1c96d78e69
Marco Bonetti <marco@cruncher.ch>
2023-02-28 11:17:35 +0100

    Django 4: ugettext_lazy was removed in favor of gettext_lazy

--- django_check_seo/cms_toolbars.py
+++ django_check_seo/cms_toolbars.py
@@ -3,7 +3,7 @@
 # Third party
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class DjangoSeoToolbar(CMSToolbar):
